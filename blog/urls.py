b5f803c7469fc64be3d6ee991b0f353e3ec7dad6
avinashdamania <avinashdamania@hotmail.com>
2017-03-05 01:44:43 -0600

    removed ursl from urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,4 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list')
-	url(r'^$', views.post_list, name='create')
-	url(r'^$', views.post_list, name='signin')
-	url(r'^$', views.post_list, name='signup')
-	url(r'^$', views.post_list, name='support')
-	url(r'^$', views.post_list, name='upload')
 ]
